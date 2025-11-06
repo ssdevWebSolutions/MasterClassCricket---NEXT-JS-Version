694a15495f55061a5aa1003f6a840f9684a31af7
shravanivibuthi <vibuthishravani@gmail.com>
2025-11-06 19:47:08 +0530

    "here is the slug for ourservices drop down sections"

---
+++ src/app/services/[slug]/page.jsx
@@ -0,0 +1,21 @@
+import ServiceLayout from '../../components/ServiceLayout';
+import servicesData from '../../data/servicesData';
+
+export default function ServicePage({ params }) {
+  const { slug } = params;
+  const service = servicesData.find((s) => s.slug === slug);
+
+  if (!service) {
+    return <h1 style={{ color: '#fff', textAlign: 'center', padding: '100px' }}>Service Not Found</h1>;
+  }
+
+  return (
+    <ServiceLayout
+      title={service.title}
+      subtitle={service.subtitle}
+      description={service.detail}
+      image={service.image}
+      expectations={service.expectations || []}
+    />
+  );
+}
