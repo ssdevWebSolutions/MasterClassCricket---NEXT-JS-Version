7292ccfa8c78c96c7eb448fad2ce8298462dc6aa
ssdevWebSolutions <ssdevwebsolutions@gmail.com>
2025-11-07 13:12:52 +0530

    "Navbar link and logo adjusted"

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -92,6 +92,7 @@ export default function Navbar() {
     { href: '/', label: 'Home' },
     { href: '/about', label: 'About Us' },
     { href: '/Founders', label: 'Meet The Owner' },
+    { href: '/masterclass_elite_cricket_academy.jsx', label: 'Elite Academy' },
     { href: '/meet-the-coaches', label: 'Meet The Coaches' }, // 👈 ADD THIS
     { href: '/testimonials', label: 'Testimonials' },
     { href: '/contact', label: 'Contact Us' }
